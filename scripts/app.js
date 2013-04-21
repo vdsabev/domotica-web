6de7242f5b6144d3da6d2fc267614ee81feacd60
Vladimir Sabev <vdsabev@gmail.com>
2013-04-22 00:06:03 +0300

    Session refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@
 
   // Settings
   app.constant('settings', {
+    delay: 200,
+    animationDuration: 400
   });
 
   // Routing
@@ -11,12 +13,13 @@
     $routeProvider
       .when('/', { controller: 'HomeController', templateUrl: 'views/content/home.html' })
       .when('/users', { controller: 'UsersController', templateUrl: 'views/content/users.html' })
+      .when('/users/:id', { controller: 'UserController', templateUrl: 'views/content/user.html' })
       .when('/systems', { controller: 'SystemsController', templateUrl: 'views/content/systems.html' })
       .otherwise({ redirectTo: '/' });
   });
 
   // Server
-  app.factory('server', function ($rootScope, $q) {
+  app.factory('server', function ($rootScope, $q, settings) {
     // Define socket and server proxy
     var socket;
     var server = {
@@ -29,19 +32,35 @@
         });
       },
       emit: function (eventName, data) {
+        var req = { data: data };
+
+        // Add session key to the request
+        var needsSessionKey = eventName !== 'destroy:session';
+        if (needsSessionKey && $rootScope.session.loggedIn) {
+          req[$rootScope.session.keyField] = $rootScope.session.key();
+        }
+
         var deferred = $q.defer();
-        socket.emit(eventName, data, function (error, result) {
+        socket.emit(eventName, req, function (error, res) {
           $rootScope.$apply(function () {
             if (error) {
+              switch (error) {
+                case 'UNAUTHORIZED':
+                case 'SESSION_EXPIRED':
+                  if ($rootScope.session.loggedIn) {
+                    $rootScope.session.destroy();
+                  }
+                  break;
+              }
               deferred.reject(error);
             }
             else {
               // Touch session
-              if (eventName !== 'refresh:session' &&
-                  eventName !== 'destroy:session') {
+              if (needsSessionKey) {
                 $rootScope.session.timestamp = new Date().getTime();
+                $rootScope.session.key(res[$rootScope.session.keyField]);
               }
-              deferred.resolve(result);
+              deferred.resolve(res.data);
             }
           });
         });
@@ -60,47 +79,33 @@
 
       key: function (value) {
         if (value !== undefined) { // Set
-          return this.data[this.keyField] = value;
+          return this[this.keyField] = value;
         }
-        return this.data[this.keyField]; // Get
+        return this[this.keyField]; // Get
       },
 
-      // Persistence
-      save: function () {
-        return $.jStorage.set('session', _.pick(this, 'data', 'loggedIn'), { TTL: this.maxExtendedLength });
-      },
+      // Local persistence
+      save: _.debounce(function () {
+        var session = _.pick(this, 'data', 'loggedIn', 'timestamp', this.keyField);
+        $.jStorage.set('session', session, { TTL: this.maxLength });
+      }, settings.delay),
       load: function () {
         return _.extend(this, $.jStorage.get('session', {}));
       },
 
       // Server operations
       create: function (credentials) {
-        return server.emit('create:session', credentials).then(function (result) {
+        return server.emit('create:session', credentials).then(function (data) {
           $rootScope.session.loggedIn = true;
-          $rootScope.session.data = result;
+          $rootScope.session.data = data;
           $rootScope.session.save();
         });
       },
       destroy: function () {
-        return server.emit('destroy:session').then(function (result) {
+        return server.emit('destroy:session').then(function () {
           $rootScope.session.loggedIn = false;
           $rootScope.session.data = {};
           $rootScope.session.save();
-        });
-      },
-      refresh: function () {
-        if (!this.key()) return; // Nothing to refresh
-
-        // Logout if session hasn't been touched in a while
-        if (new Date().getTime() - this.timestamp > this.maxLength) {
-          return this.destroy();
-        }
-
-        var session = {};
-        session[this.keyField] = this.key();
-        return server.emit('refresh:session', session).then(function (key) {
-          $rootScope.session.key(key);
-          _.delay($rootScope.session.refresh, $rootScope.session.maxLength / 2);
         });
       }
     };
@@ -115,8 +120,13 @@
     socket.on('connect', function () {
       if ($rootScope.session.loggedIn) {
         $rootScope.session.timestamp = new Date().getTime();
-        $rootScope.session.refresh().then(function (key) {
-          $rootScope.session.key(key);
+        server.emit('refresh:session').then(function () {
+          $rootScope.connected = true;
+        });
+      }
+      else {
+        $rootScope.$apply(function() {
+          $rootScope.connected = true;
         });
       }
     });
--- scripts/controllers.js
+++ scripts/controllers.js
@@ -6,6 +6,29 @@
       };
     },
 
+    HomeController: function ($scope) {
+    },
+
+    // Users
+    UsersController: function ($scope, server) {
+      server.emit('get:users').then(function (data) {
+        $scope.users = data;
+      });
+    },
+    UserController: function ($scope, $routeParams, server) {
+      server.emit('get:user', { _id: $routeParams.id }).then(function (data) {
+        $scope.user = data;
+      });
+    },
+
+    // Systems
+    SystemsController: function ($scope, server) {
+      server.emit('get:systems').then(function (data) {
+        $scope.systems = data;
+      });
+    },
+
+    // Misc
     LoginController: function ($scope) {
       $scope.login = function () {
         $scope.session.create($scope.credentials).then(function () {
@@ -17,19 +40,6 @@
       $scope.register = function () {
         server.emit('create:user', _.pick($scope.registration, 'name', 'email', 'password'));
       };
-    },
-
-    HomeController: function ($scope) {
-    },
-    UsersController: function ($scope, server) {
-      server.emit('get:users').then(function (result) {
-        $scope.users = result;
-      });
-    },
-    SystemsController: function ($scope, server) {
-      server.emit('get:systems').then(function (result) {
-        $scope.systems = result;
-      });
     }
   });
 }(angular));
